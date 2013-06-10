e61756b4947eb8d1d6615f6f0c0d1db1d3b9d9b2
markeganfuller <mark.egan-fuller@mvfglobal.com>
2013-06-10 14:07:14 +0100

    Tidied up, changed names as the spark graphs aren't actually ASCII. Added Horizontal graphs

--- ascii_sparks.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-parts = u' ▁▂▃▄▅▆▇▉'
-
-def sparks(nums):
-    fraction = max(nums) / float(len(parts) - 1)
-    return ''.join(parts[int(round(x/fraction))] for x in nums)
-
-
---
+++ textgraph.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+'''
+Text Graphs
+'''
+
+
+def vertical_spark(nums):
+    '''
+    Returns a vertical spark graph from
+    the list of integers num
+    '''
+    parts = u' ▁▂▃▄▅▆▇▉'
+    fraction = max(nums) / float(len(parts) - 1)
+    return ''.join(parts[int(round(x / fraction))] for x in nums)
+
+
+def horizontal_graph(nums, width=79):
+    '''
+    Returns a horizontal spark graph from
+    the list of integers num of width width
+    '''
+    parts = ['▉' * i for i in range(0, width)]
+    fraction = max(nums) / float(len(parts) - 1)
+    return ''.join(parts[int(round(x / fraction))] + "\n" for x in nums)
+
+
+if __name__ == "__main__":
+    example = [12, 34, 45, 5, 16, 20]
+    print "Vertical Spark"
+    print vertical_spark(example)
+    print "Horizontal Spark"
+    print horizontal_graph(example)
+    print "Horizontal Spark, Width 20"
+    print horizontal_graph(example, 20)
