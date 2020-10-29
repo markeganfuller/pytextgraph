6288efcae2e0a543359d1723ee1d7a35f46596d8
Mark Egan-Fuller <mark.eganfuller@bytemark.co.uk>
2020-10-29 20:50:27 +0000

    Expand travis tests to include OSX and Windows.

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
 """Setup File."""
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='textgraph',
-    version='1.0.1',
+    version='2.0.0',
+    author='Mark Egan-Fuller',
+    author_email='markeganfuller@googlemail.com',
     description='Library for creating text based graphs',
-    long_description=('Library for creating text based graphs, '
-                      'including spark and bar graphs'),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/markeganfuller/pytextgraph',
     classifiers=[
         'Programming Language :: Python :: 3 :: Only',
         'Intended Audience :: Developers',
         ('License :: OSI Approved :: GNU General '
-         'Public License v3 or later (GPLv3+)')
+         'Public License v3 or later (GPLv3+)'),
+        'Operating System :: OS Independent',
     ],
     keywords='text graph textgraph asciigraph',
-    url='https://github.com/markeganfuller/pytextgraph',
-    author='Mark Egan-Fuller',
-    author_email='markeganfuller@googlemail.com',
     license='GPLv3+',
     packages=['textgraph'],
-    zip_safe=False,
 )
